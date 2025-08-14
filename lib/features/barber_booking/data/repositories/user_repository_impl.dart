a9a19bff0c72aec2037881e35be7310d4d9114ad
Luiz <luizdevelopment7@hotmail.com>
2025-08-14 12:23:06 -0300

    Adiciona user_repository_impl.dart

---
+++ lib/features/barber_booking/data/repositories/user_repository_impl.dart
@@ -0,0 +1,14 @@
+import 'package:barber_schedule/features/barber_booking/domain/entities/user_entity.dart';
+import 'package:barber_schedule/features/barber_booking/domain/repositories/user_repositories.dart';
+
+class UserRepositoryImpl implements UserRepository {
+  final UserRemoteDataSource remoteDataSource;
+  UserRepositoryImpl(this.remoteDataSource);
+
+  @override
+  @override
+  Future<UserEntity?> getUserByEmail(String email) async {
+    final userModel = await remoteDataSource.getUserByEmail(email);
+    return userModel?.toEntity();
+  }
+}
